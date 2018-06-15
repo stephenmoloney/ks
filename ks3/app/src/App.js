0db2cf09546bf3b222387044ab440b4fa1767813
Stephen Moloney <stephen@stephenmoloney.com>
2018-06-15 09:45:37 +0100

    make react component do some re-rendering

--- ks3/app/src/App.js
+++ ks3/app/src/App.js
@@ -2,32 +2,25 @@ import React, { Component } from 'react'
 import './App.css'
 import 'whatwg-fetch'
 
-const fetchMessage =
-  function(){
-    fetch('/api/hello', {
-     headers: {
-       "Content-Type": "application/json"
-     }
-    }).then(response => {
-      return response.json()
-    }).then(json => {
-      return { message: json.message[0] || 'there' }
-    })
-  }
-
 class App extends Component {
 
   constructor(props){
     super(props)
     this.state = { message: 'there' };
-
   }
 
   componentDidMount() {
-    this.interval = setInterval(() =>
-      fetchMessage()
-      .then(newState => {
-        this.setState(newState)
+    var self = this;
+
+    self.interval = setInterval(() =>
+      fetch('/api/hello', {
+        headers: {"Content-Type": "application/json"}
+      }).then(response => {
+        return response.json()
+      }).then(json => {
+        return { message: json.message[0] || 'there' }
+      }).then(newState => {
+        self.setState(newState)
       })
     , 1000);
   }
